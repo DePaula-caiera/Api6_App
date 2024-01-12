40617175e3d6d80ac671ecc683b4b65def45c3c6
Caiera03 <caiodepaula90@hotmail.com>
2024-01-11 22:33:23 -0300

    teste commit

--- obj/Debug/net6.0/IWantApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/IWantApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192cdf985cf1ebf1802fc6d06679b7b66fcafcca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
