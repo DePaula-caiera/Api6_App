cb0f36f4a11a87dc08493995ce03670f121f9479
Caiera03 <caiodepaula90@hotmail.com>
2024-02-27 17:24:11 -0300

    Aula 87.

--- Endpoints/Empregados/EmpregadoGetAll.cs
+++ Endpoints/Empregados/EmpregadoGetAll.cs
@@ -1,6 +1,9 @@
 ﻿using Dapper;
+using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Net;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Empregados;
@@ -11,21 +14,35 @@ public class EmpregadoGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int? page, int? rows, IConfiguration configuration)
-    {
-        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
-        var query =
-            @"SELECT Email, ClaimValue AS NOME  
-            FROM AspNetUsers AS U
-            INNER JOIN AspNetUserClaims C ON U.Id = C.UserId AND C.ClaimType = 'Nome'
-            ORDER BY Email
-            OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
-        var empregados = db.Query<EmpregadoResponse>(
-            query,
-            new { page, rows }
-        );
+  
 
-        return Results.Ok(empregados);
+    public static IResult Action(int? page, int? rows, QueryAllUsersWithClaimName query)
+    {
+
+       
+            //if (page == null || rows == null)
+            //    return Results.BadRequest("Ausência de Parâmetros");
+
+            //int.TryParse(page, out var pageId);
+            //int.TryParse(rows, out var rowsId);
+            //var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+            //var query =
+            //    @"SELECT Email, ClaimValue AS NOME  
+            //    FROM AspNetUsers AS U
+            //    INNER JOIN AspNetUserClaims C ON U.Id = C.UserId AND C.ClaimType = 'Nome'
+            //    ORDER BY Email
+            //    OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+            //var empregados = db.Query<EmpregadoResponse>(
+            //    query,
+            //    new { pageId, rowsId }
+            //);
+
+            //if(!empregados.Any()) 
+            //    return Results.NotFound();
+
+            return Results.Ok(query.Execute(page.Value, rows.Value));        
         
     }
+  
 }
+
---
+++ Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using IWantApp.Endpoints.Empregados;
+using Microsoft.Data.SqlClient;
+
+namespace IWantApp.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+    private IConfiguration configuration;
+
+    public QueryAllUsersWithClaimName(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public IEnumerable<EmpregadoResponse> Execute(int page, int rows)
+    {        
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query =
+            @"SELECT Email, ClaimValue AS NOME  
+                FROM AspNetUsers AS U
+                INNER JOIN AspNetUserClaims C ON U.Id = C.UserId AND C.ClaimType = 'Nome'
+                ORDER BY Email
+                OFFSET (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+        return db.Query<EmpregadoResponse>(
+            query,
+            new { page, rows }
+        );
+    }
+}
